95125667c9b216ccb64792ef67a90a97b6622ec3
nghiahsgs <nghiahsgs@gmail.com>
2020-03-29 01:53:32 +0700

    make it better

---
+++ ge stock/index.py
@@ -0,0 +1,5 @@
+# scrollTo=on0uU_PMSf-b
+#https: // colab.research.google.com/drive/16N7PgPEFinOHu6Rkjvx0j8ckgsAibvW5
+https: // towardsdatascience.com/predicting-stock-price-with-lstm-13af86a74944
+
+https: // github.com/DarkKnight1991/Stock-Price-Prediction
